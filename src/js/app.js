76068556d231b77387e5439536cb9da87654beae
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-07-01 16:20:16 +1000

    Test

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,6 @@ import defineHealthStatus from './basic';
 
 console.log('app worked');
 console.log('app worked');
+console.log('app worked');
 
 console.log(defineHealthStatus({ name: 'Маг', health: 90 }));
